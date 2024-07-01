e82300670d135a6ece349340423c48750d662781
saifnihad300 <hossainnihad300@gmail.com>
2024-07-01 12:32:44 +0600

    today add

---
+++ 01. Introduction to JavaScript/JS with Simanto Paul/script.js
@@ -0,0 +1,9 @@
+console.log("This is First Line");
+console.log(("This is Second Line"));
+console.log(("This is Third Line"));
+document.write("This is Fourth Line");
+
+var x;
+x = prompt("Enter Your Name");
+console.log(x);
+document.write(x);
